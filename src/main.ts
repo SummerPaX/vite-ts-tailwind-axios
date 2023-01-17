03223ec933ad2c61b1753b140dae6899e725bfd4
Paulus Summer <paulus.summer@webundsoehne.com>
2023-01-17 13:04:37 +0100

    no ned fertig

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import axios from 'axios'
 
 const title = document.getElementById("title")
+const searchForm = document.getElementById("searchForm")
+const queryInput = document.getElementById("queryInput")
+const beerElement = document.getElementById("beer")
 
-if (title) title.textContent = "Hello Vite!"
+if (title) title.textContent = "Looking for Beer?"
+
+searchForm?.addEventListener('submit', () => {
+
+})
